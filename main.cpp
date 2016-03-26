83a8d162ab6c184d8eb6af33ec467dd35e9d3132
tildearrow <tildearrow@users.noreply.github.com>
2016-03-25 20:09:24 -0500

    adding all scrolling code

--- main.cpp
+++ main.cpp
@@ -21,52 +21,105 @@
  */
 #include <stdio.h>
 #include <vector>
+#include <queue>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
 struct gchar {
   int character, x;
+  unsigned char r, g, b;
+  unsigned char bold, underline, stroke, conceal;
 };
 
 std::vector<gchar> chars;
+std::queue<int> charq;
 int gx, gy, gw, gh;
 int pc1, pc2;
 int fc;
+int counter;
+int popped;
+int speed;
 SDL_Window* window;
 SDL_Renderer* r;
 SDL_Surface* texts;
 SDL_Texture* textt;
+SDL_Thread* thread;
 TTF_Font* font;
 SDL_Rect reeect;
+SDL_Color color={255,255,255,255};
 
-int gputchar(int x, int y, int c) {
-    texts=TTF_RenderGlyph_Blended(font,c,{255,255,255,255});
+int gputchar(int x, int y, int c, bool actuallyrender) {
+    texts=TTF_RenderGlyph_Blended(font,c,color);
     textt=SDL_CreateTextureFromSurface(r,texts);
     reeect.x=x;
     reeect.y=y;
     reeect.w=texts->clip_rect.w;
     reeect.h=texts->clip_rect.h;
-    SDL_RenderCopy(r,textt,&texts->clip_rect,&reeect);
+    if (actuallyrender) {
+      SDL_RenderCopy(r,textt,&texts->clip_rect,&reeect);
+    }
     SDL_FreeSurface(texts);
     SDL_DestroyTexture(textt);
+    return reeect.w;
+}
+
+static int inthread(void* ptr) {
+  char chaar;
+  while (true) {
+    chaar=getchar();
+    printf("%d\n",chaar);
+    if (chaar!='\n') {
+      charq.push(chaar);
+    }
+  }
+  return 0;
 }
 
 int main(int argc, char** argv) {
-  gx=0; gy=0; gw=1920; gh=32; fc=0;
+  gx=0; gy=0; gw=1920; gh=32; fc=0; counter=4; speed=3;
   printf("usage: %s FONT SIZE\n",argv[0]);
   SDL_Init(SDL_INIT_VIDEO);
   TTF_Init();
   font=TTF_OpenFont(argv[1],atoi(argv[2]));
   window=SDL_CreateWindow("scroller",gx,gy,gw,gh,0);
   r=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+  thread=SDL_CreateThread(inthread,"inthread",NULL);
   while (true) {
     SDL_RenderClear(r);
-    gputchar(fc,0,'a');
+    for (int i=0; i<fmax(1,speed); i++) {
+      if (counter==0) {
+	if (charq.size()>0) {
+	  speed=3;
+	  popped=charq.front();
+	  charq.pop();
+	  chars.resize(chars.size()+1);
+	  chars[chars.size()-1].x=gw; chars[chars.size()-1].character=popped;
+	  counter=gputchar(0,0,popped,false);
+	} else {
+	  speed=0;
+	}
+      } else {
+	counter--;
+      }
+      if (speed>0) {
+	for (int i=0; i<chars.size(); i++) {
+	  chars[i].x-=1;
+	}
+      }
+    }
+    for (std::vector<gchar>::iterator i=chars.begin(); i!=chars.end(); i++) {
+      gputchar(i->x,0,i->character,true);
+      if (i->x<-128) { // hehe
+	chars.erase(i);
+	printf("char removed!\n");
+      }
+    }
+    
+    
     SDL_RenderPresent(r);
     ++fc;
     pc2=pc1;
     pc1=SDL_GetTicks();
-    printf("%d\n",pc1-pc2);
   }
   return 0;
 }
